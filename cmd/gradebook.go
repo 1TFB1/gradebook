00d8628c70c4f9cbe7a1bc18c9e6ad7807f3ec6a
1TFB1 <135456155+1TFB1@users.noreply.github.com>
2025-11-29 14:03:40 +0100

    Update gradebook.go

--- cmd/gradebook.go
+++ cmd/gradebook.go
@@ -5,7 +5,37 @@ import (
 	"github.com/urfave/cli/v3"
 )
 
-func main() {
+func main(){
+	cmd := &cli.Command{
+		Name:  "gradebook",
+		Usage: "Gradebook displays current grades",
+		Flags: []cli.Flag{
+			&cli.IntFlag{
+				Name:  "stOcen",
+				Usage: "najmanjse stevilo ocen potrebnih za pozitivno ocen",
+				Value: 5,
+			},
+			&cli.IntFlag{
+				Name:  "minOcena",
+				Usage: "Minimalna mozna ocena za dodajanje",
+				Value: 6,
+			},
+			&cli.IntFlag{
+				Name:  "maxOcena",
+				Usage: "Maksimalna mozna ocena za dodajanje",
+				Value: 10,
+			},
+		},
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			min := cmd.Int("minOcena")
+			max := cmd.Int("maxOcena")
+			st := cmd.Int("stOcen")
+			return run(stOcen, minOcena, maxOcena)
+		},
+	}
+}
+
+func run(stOcen int, minOcena int, maxOcena int) {
 
 	Janez := Student{"Janez", "Novak", []int{5,6,7}}
 	Mojca := Student{"Mojca", "Novak", []int{10,10,10,10,10,10}}
